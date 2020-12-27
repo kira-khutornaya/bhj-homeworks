4e4837e0ef7b8dd156141486035f9dd0e052595a
Kira Khutornaya <kira.khutornaya@mail.ru>
2020-12-27 12:13:17 +0300

    first commit popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const modal = document.getElementById("modal_main");
+const close = [...document.getElementsByClassName("modal__close")];
+const success = document.querySelector(".show-success");
+const modal_success = document.getElementById("modal_success");
+
+modal.classList.add("modal_active");
+
+close.forEach(el => {
+    el.onclick = () => {
+        modal.classList.remove("modal_active");
+        modal_success.classList.remove("modal_active");
+    }
+})
+
+success.onclick = () => modal_success.classList.add("modal_active");
