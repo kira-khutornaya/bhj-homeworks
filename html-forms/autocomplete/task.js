a0a3d6e50e03bb03fb189b8f7a87a01d0c3cd135
Kira Khutornaya <kira.khutornaya@mail.ru>
2021-01-25 21:13:51 +0300

    first commit autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,17 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    const words = [...this.input.options];
+    const wordsFilter = words.filter(el => {
+      if (el.text.includes(text)) {
+        return {
+          text: el.text,
+          value: el.value
+        }
+      }
+    })
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    return wordsFilter;
   }
 }
 
